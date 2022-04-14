3a6756e5e4293af18f2990907dfa9d62089d53fb
Vinicius de Moraes <viniciusmoraeslima@gmail.com>
2022-04-14 11:11:47 -0300

    novo gitignore

--- DiscordBOT/BOT.cs
+++ DiscordBOT/BOT.cs
@@ -1,13 +1,14 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace DiscordBOT
 {
-    class Program
+    class BOT
     {
-		public static Task Main(string[] args) => new Program().MainAsync();
+		public static Task Main(string[] args) => new BOT().MainAsync();
 
 		private DiscordSocketClient _client;
 		public async Task MainAsync()
@@ -15,7 +16,9 @@ namespace DiscordBOT
 			_client = new DiscordSocketClient();
 			_client.Log += Log;
 
-			var token = ;
+
+			//Configurar o Token.TXT para autenticação do BOT.
+			var token = File.ReadAllText("token.txt");
 
 			await _client.LoginAsync(TokenType.Bot, token);
 			await _client.StartAsync();
